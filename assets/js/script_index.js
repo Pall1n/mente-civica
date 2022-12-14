ee121ac4ab9d0fecfb7c25af75131c8ed5a010e7
Pall1n <paolomarialarosa@gmail.com>
2022-12-14 20:16:06 +0100

    Dynamic elements adding

--- assets/js/script_index.js
+++ assets/js/script_index.js
@@ -1,23 +1,37 @@
-var topic;
+var topic, json = $.getJSON("./hierarchy.json");
 
-$(".home_button").click(function() {
-    topic = $(this).attr("id");
-    console.log(topic);
-    $("#buttons").load("./views/" + topic + "/buttons.html", function(){
-        $(".view_button").click(function() {
-            $(".view_button").removeClass("disabled");
-            view = $(this).attr("id");
-            var view = $(this).attr("id");
-            changeFrame(topic, view);
-            $(this).addClass("disabled");
+function homeClickButton() {
+    $(".home_button").click(function() {
+        topic = $(this).attr("id");
+        json.done(function(data) {
+            $.each(data[topic]["children"], function(key, value) {
+                $("#buttonsDiv").empty().append('<a id="' + key + '" class="btn view_button" role="button">' + value + '</a>');
+            });
+            viewClickButton();
         });
     });
-});
+}
+
+function viewClickButton() {
+    $(".view_button").click(function() {
+        $(".view_button").removeClass("disabled");
+        view = $(this).attr("id");
+        var view = $(this).attr("id");
+        changeFrame(topic, view);
+        $(this).addClass("disabled");
+    });
+}
 
 function changeFrame(topic, view) {
     $("#viewFrame").load("./views/" + topic + "/" + view + ".html");
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-	AOS.init();
+    json.done(function(data) {
+        $.each(data, function(key, value) {
+            $("#buttonsDiv").append('<a id="' + key + '" class="btn home_button" role="button">' + value["name"] + '</a>');
+        });
+        homeClickButton();
+    });
+    AOS.init();
 }, false);
