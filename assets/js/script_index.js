6ea74541227e6a0a5f88e45cd9b3d6a2b35a1c1f
Pall1n <paolomarialarosa@gmail.com>
2022-12-14 17:50:13 +0100

    GG

--- assets/js/script_index.js
+++ assets/js/script_index.js
@@ -1,14 +1,23 @@
-$(".view_button").click(function() {
-    var view = $(this).attr("id");
-    changeFrame(view);
-    $(this).addClass("disabled");
+var topic;
+
+$(".home_button").click(function() {
+    topic = $(this).attr("id");
+    console.log(topic);
+    $("#buttons").load("./views/" + topic + "/buttons.html", function(){
+        $(".view_button").click(function() {
+            $(".view_button").removeClass("disabled");
+            view = $(this).attr("id");
+            var view = $(this).attr("id");
+            changeFrame(topic, view);
+            $(this).addClass("disabled");
+        });
+    });
 });
 
-$(".menu_button").click(function() {
-    var topic = $(this).attr("id");
-    window.location = "./views/"+topic+"/index.html";
-});
-
-function changeFrame(view) {
-    $("#viewFrame").load("./views/" + view + ".html");
+function changeFrame(topic, view) {
+    $("#viewFrame").load("./views/" + topic + "/" + view + ".html");
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+	AOS.init();
+}, false);
